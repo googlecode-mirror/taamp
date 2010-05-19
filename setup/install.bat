95069761e924f8b739d480c5f622d7d5b8d516de
partybase <partybase@018c7de8-08ff-d85f-ed61-2f42cfa0e229>
2010-05-19 02:08:35 +0000

    安装时启动托盘控制器,并提示用户访问控制器的路径

--- setup/install.bat
+++ setup/install.bat
@@ -7,6 +7,11 @@ sc create TaAMP.Svn binpath= "%TaAMPServerPath%\local\svn\bin\svnserve.exe --ser
 net start TaAMP.Mysql
 net start TaAMP.Apache
 net start TaAMP.Svn
+start %TaAMPServerPath%\Monitor.exe
+echo 你已经成功安装TaAMP服务器套件,为了方便你的管理,请通过
+echo %TaAMPServerPath%\Monitor.exe 
+echo 进行对Apache和Mysql进行开关操作
+PAUSE
 start http://localhost/phpinfo.php
 start %TaAMPServerPath%\readme.txt
 PAUSE
