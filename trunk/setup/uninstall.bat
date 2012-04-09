efb3f6c71337f39c24119451f2c945b40e28edde
partybase@gmail.com <partybase@gmail.com@018c7de8-08ff-d85f-ed61-2f42cfa0e229>
2012-04-09 10:05:53 +0000

    增加ZendOptimizer 3.3.0 增加memcached 1.4.0 增加php_memcache.dll 2.2.5

--- trunk/setup/uninstall.bat
+++ trunk/setup/uninstall.bat
@@ -1,10 +1,9 @@
 @echo OFF
-@set TaAMPServerPath=%cd:~0,-6%
+@set TaAMPServerPath=%~dp0
+@set TaAMPServerPath=%TaAMPServerPath:~ 0,-6%
+(sc query TaAMP.Mysql | find "TaAMP.Mysql">nul && net stop TaAMP.Mysql && "%TaAMPServerPath%\local\mysql\bin\mysqld.exe" -remove Taamp.Mysql)
+(sc query TaAMP.Apache | find "TaAMP.Apache">nul && net stop TaAMP.Apache && "%TaAMPServerPath%\local\apache\bin\httpd.exe" -k uninstall -n Taamp.Apache)
+(sc query TaAMP.Svn | find "TaAMP.Svn">nul && net stop TaAMP.Svn && sc delete TaAMP.Svn)
+(sc query TaAMP.Memcached | find "TaAMP.Memcached">nul && net stop TaAMP.Memcached && sc delete TaAMP.Memcached)
 
-net stop TaAMP.Mysql
-net stop TaAMP.Apache
-net stop TaAMP.Svn
-"%TaAMPServerPath%\local\mysql\bin\mysqld.exe" -remove Taamp.Mysql
-"%TaAMPServerPath%\local\apache\bin\httpd.exe" -k uninstall -n Taamp.Apache
-sc delete TaAMP.Svn
 PAUSE
