ea278cfb1b58e9b154e7988891d2d97d8cd03678
partybase@gmail.com <partybase@gmail.com@018c7de8-08ff-d85f-ed61-2f42cfa0e229>
2012-04-10 03:29:30 +0000

    优化安装及卸载脚本 增加memcache.php的测试脚本

--- trunk/setup/install.bat
+++ trunk/setup/install.bat
@@ -1,6 +1,17 @@
 @echo OFF
 @set TaAMPServerPath=%~dp0
 @set TaAMPServerPath=%TaAMPServerPath:~ 0,-6%
+echo ┌──────────────────────────┐
+echo │ 准备安装 TaAMP服务器套件                           │
+echo │ 在安装的过程中，可能会有杀毒软件提示不安全操作     │
+echo │ 如：调用 sc 命令（系统服务的操作命令）。           │
+echo │ 请选择允许否则本套件程序将不能正常运行。           │
+echo │ 本套件程序只是为了方便初学者能快速配置服务器程序。 │
+echo └──────────────────────────┘
+set input=
+set /p input=如果明白，请输入ok:
+echo %input%
+if not "x%input%"=="xok" goto exit_install
 
 rem install Apache
 "%TaAMPServerPath%\local\apache\bin\httpd.exe" -k install -n TaAMP.Apache
@@ -25,8 +36,17 @@ sc create TaAMP.Memcached binpath= "%TaAMPServerPath%\local\memcached\srvany.exe
 (sc query TaAMP.Memcached | find "TaAMP.Memcached">nul && net start TaAMP.Memcached)
 
 
+
+
+:final_install
 echo 你已经成功安装TaAMP服务器套件,为了方便你的管理,请通过
 echo %TaAMPServerPath%\Monitor.exe
 echo 进行对Apache和Mysql进行开关操作
 start %TaAMPServerPath%\readme.txt
 PAUSE
+exit
+
+:exit_install
+echo 退出安装程序
+pause
+exit
