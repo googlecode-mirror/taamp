0d947620454ab8e31e154800fdaf1ef64017de8c
partybase <partybase@gmail.com>
2012-04-11 09:41:55 +0000

    如果点击 install.bat 时，先执行 uninstall.bat ,保证没有已经安装的程序。再进行安装。

--- setup/uninstall.bat
+++ setup/uninstall.bat
@@ -3,13 +3,12 @@
 @set TaAMPServerPath=%TaAMPServerPath:~ 0,-6%
 
 rem Í£Ö¹ TaAMP.Svn £¬È»ºóÐ¶ÔØ TaAMP.Svn
-(sc query TaAMP.Mysql | find "4  RUNNING">nul && net stop TaAMP.Mysql)
-(sc query TaAMP.Mysql | find "TaAMP.Mysql">nul && "%TaAMPServerPath%\local\mysql\bin\mysqld.exe" -remove Taamp.Mysql)
-
-
-rem Í£Ö¹ TaAMP.Svn £¬È»ºóÐ¶ÔØ TaAMP.Svn
 (sc query TaAMP.Apache | find "4  RUNNING">nul && net stop TaAMP.Apache)
 (sc query TaAMP.Apache | find "TaAMP.Apache">nul && "%TaAMPServerPath%\local\apache\bin\httpd.exe" -k uninstall -n Taamp.Apache)
+
+rem Í£Ö¹ TaAMP.Svn £¬È»ºóÐ¶ÔØ TaAMP.Svn
+(sc query TaAMP.Mysql | find "4  RUNNING">nul && net stop TaAMP.Mysql)
+(sc query TaAMP.Mysql | find "TaAMP.Mysql">nul && "%TaAMPServerPath%\local\mysql\bin\mysqld.exe" -remove Taamp.Mysql)
 
 rem Í£Ö¹ TaAMP.Svn £¬È»ºóÐ¶ÔØ TaAMP.Svn
 (sc query TaAMP.Svn | find "4  RUNNING">nul && net stop TaAMP.Svn)
@@ -20,4 +19,4 @@ rem Í£Ö¹ TaAMP.Memcached £¬È»ºóÐ¶ÔØ TaAMP.Memcached
 (sc query TaAMP.Memcached | find "4  RUNNING">nul && net stop TaAMP.Memcached)
 (sc query TaAMP.Memcached | find "TaAMP.Memcached">nul &&sc delete TaAMP.Memcached)
 
-PAUSE
+if not "x%1"=="xfrom_install" PAUSE
